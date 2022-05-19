0e4074d043c486242905dbe06feaccc8ed80585e
Emcikem <18968479985@163.com>
2022-05-19 21:30:27 +0800

    维护transport和负载均衡单例池

--- http_proxy_middleware/http_load_balancer.go
+++ http_proxy_middleware/http_load_balancer.go
@@ -16,15 +16,20 @@ import (
 var LoadBalancerHandler *LoadBalancer
 
 type LoadBalancer struct {
-	LoadBalanceMap   map[string]load_balance.LoadBalance
-	LoadBalanceSlice []load_balance.LoadBalance
+	LoadBalanceMap   map[string]*LoadBalancerItem
+	LoadBalanceSlice []*LoadBalancerItem
 	Locker           sync.RWMutex
+}
+
+type LoadBalancerItem struct {
+	LoadBalance load_balance.LoadBalance
+	ServiceName string
 }
 
 func NewLoadBalancer() *LoadBalancer {
 	return &LoadBalancer{
-		LoadBalanceMap:   map[string]load_balance.LoadBalance{},
-		LoadBalanceSlice: []load_balance.LoadBalance{},
+		LoadBalanceMap:   map[string]*LoadBalancerItem{},
+		LoadBalanceSlice: []*LoadBalancerItem{},
 		Locker:           sync.RWMutex{},
 	}
 }
@@ -32,7 +37,7 @@ func NewLoadBalancer() *LoadBalancer {
 var TransportHandler *Transportor
 
 type Transportor struct {
-	TransportMap   map[string]TransportItem
+	TransportMap   map[string]*TransportItem
 	TransportSlice []*TransportItem
 	Locker         sync.RWMutex
 }
@@ -44,24 +49,31 @@ type TransportItem struct {
 
 func NewTransportor() *Transportor {
 	return &Transportor{
-		TransportMap:   map[string]TransportItem{},
+		TransportMap:   map[string]*TransportItem{},
 		TransportSlice: []*TransportItem{},
 		Locker:         sync.RWMutex{},
 	}
 }
 
 func init() {
-	LoadBalancerHandler = NewLoadBalancer()
+	LoadBalancerHandler = NewLoadBalancer() // 单例的负载均衡池
+	TransportHandler = NewTransportor()     // 单例的transport池
 }
 
-func (l *LoadBalancer) GetLoadBalance(gatewayService *dao.GatewayService) (load_balance.LoadBalance, error) {
+func (lbr *LoadBalancer) GetLoadBalance(gatewayService *dao.GatewayService) (load_balance.LoadBalance, error) {
+	for _, lbrItem := range lbr.LoadBalanceSlice {
+		if lbrItem.ServiceName == gatewayService.ServiceName {
+			return lbrItem.LoadBalance, nil
+		}
+	}
+
 	httpRule := model.GatewayServiceHttpRuleVO{}
 	err := json.Unmarshal([]byte(gatewayService.RemoteParams), &httpRule)
 	if err != nil {
 		return nil, err
 	}
 	schema := "http"
-	if httpRule.NeedHttps == 1 {
+	if httpRule.NeedHttps == 1 { // 支持https
 		schema = "https"
 	}
 	prefix := ""
@@ -76,6 +88,7 @@ func (l *LoadBalancer) GetLoadBalance(gatewayService *dao.GatewayService) (load_
 		ipConf[ipItem] = weightList[ipIndex]
 	}
 
+	// 构建服务发现版负载均衡
 	mConf, err := load_balance.NewLoadBalanceCheckConf(
 		fmt.Sprintf("%s://%s%s", schema, prefix),
 		ipConf,
@@ -83,8 +96,18 @@ func (l *LoadBalancer) GetLoadBalance(gatewayService *dao.GatewayService) (load_
 	if err != nil {
 		return nil, err
 	}
-	return load_balance.LoadBalanceFactoryWithConf(
-		load_balance.LbRandom, mConf), nil
+	lb := load_balance.LoadBalanceFactoryWithConf(load_balance.LbRandom, mConf)
+
+	// 维护单例的负载均衡池
+	lbItem := &LoadBalancerItem{
+		LoadBalance: lb,
+		ServiceName: gatewayService.ServiceName,
+	}
+	lbr.LoadBalanceSlice = append(lbr.LoadBalanceSlice, lbItem)
+	lbr.Locker.Lock()
+	defer lbr.Locker.Unlock()
+	lbr.LoadBalanceMap[gatewayService.ServiceName] = lbItem
+	return lb, nil
 }
 
 func (t *Transportor) GetTrans(gatewayService *dao.GatewayService) (*http.Transport, error) {
@@ -93,7 +116,11 @@ func (t *Transportor) GetTrans(gatewayService *dao.GatewayService) (*http.Transp
 	if err != nil {
 		return nil, err
 	}
-
+	for _, transItem := range t.TransportSlice {
+		if transItem.ServiceName == gatewayService.ServiceName {
+			return transItem.Trans, nil
+		}
+	}
 	trans := &http.Transport{
 		DialContext: (&net.Dialer{
 			Timeout: time.Duration(httpRule.UpstreamConnectTimeout),
@@ -102,9 +129,13 @@ func (t *Transportor) GetTrans(gatewayService *dao.GatewayService) (*http.Transp
 		IdleConnTimeout:       time.Duration(httpRule.UpstreamIDleTimeout),
 		ResponseHeaderTimeout: time.Duration(httpRule.UpstreamHeaderTimeout),
 	}
-	t.TransportSlice = append(t.TransportSlice, &TransportItem{
+	transItem := &TransportItem{
 		Trans:       trans,
 		ServiceName: gatewayService.ServiceName,
-	})
+	}
+	t.TransportSlice = append(t.TransportSlice, transItem)
+	t.Locker.Lock()
+	defer t.Locker.Unlock()
+	t.TransportMap[gatewayService.ServiceName] = transItem
 	return trans, nil
 }
